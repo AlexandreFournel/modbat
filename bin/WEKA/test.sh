4db961114213dbc1a42cf6946e1dfe25c2662b8e
Alexandre Fournel <alexandrefournel@n152-p119.eduroam.kth.se>
2019-07-05 12:32:11 +0200

    word list done

--- bin/WEKA/test.sh
+++ bin/WEKA/test.sh
@@ -1,36 +1,54 @@
 #!/bin/bash
-FILE_WITHOUT_MAIN_PATH="ERROR"
-NAME_FILE="ERROR"
-FOLDER="ERROR"
-NAME_FILE_WITHOUT_SLASH="ERROR"
-(find ../../log -name "*.err" -o -name "*.log") | while read FILE; do
+#FILE_WITHOUT_MAIN_PATH="ERROR"
+#NAME_FILE="ERROR"
+#FOLDER="ERROR"
+#NAME_FILE_WITHOUT_SLASH="ERROR"
+#(find ../../log -name "*.err" -o -name "*.log") | while read FILE; do
     
-    FILE_WITHOUT_MAIN_PATH=${FILE/..\/..\/log/}
+#    FILE_WITHOUT_MAIN_PATH=${FILE/..\/..\/log/}
     
-    if [[ $FILE_WITHOUT_MAIN_PATH =~ "/config/" ]]; then
-        FOLDER="Files/config/"
-        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/config\//}
+#    if [[ $FILE_WITHOUT_MAIN_PATH =~ "/config/" ]]; then
+#        FOLDER="Files/config/"
+#        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/config\//}
     
-    elif [[ $FILE_WITHOUT_MAIN_PATH =~ "/modbat/" ]]; then 
-        FOLDER="Files/modbat/"
-        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/modbat\//}
-    else
-        FOLDER="Files/modbat.config.ConfigTest/"
-        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/modbat.config.ConfigTest\//}
-    fi
-    mkdir -p $FOLDER;
-    NAME_FILE_WITHOUT_SLASH=${NAME_FILE//\//\_}
-    grep '^.DEBUG' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.debug 
-    grep '^.INFO' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.info 
-    grep '^.FINE' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.fine 
-    grep '^.ERROR' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.error 
-    grep '^.WARNING' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.warning
-    grep -v '^\[' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.raw 
-done
+#    elif [[ $FILE_WITHOUT_MAIN_PATH =~ "/modbat/" ]]; then 
+#        FOLDER="Files/modbat/"
+#        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/modbat\//}
+#    else
+#        FOLDER="Files/modbat.config.ConfigTest/"
+#        NAME_FILE=${FILE_WITHOUT_MAIN_PATH/\/modbat.config.ConfigTest\//}
+#    fi
+#    mkdir -p $FOLDER;
+#    NAME_FILE_WITHOUT_SLASH=${NAME_FILE//\//\_}
+#    grep '^.DEBUG' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.debug 
+#    grep '^.INFO' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.info 
+#    grep '^.FINE' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.fine 
+#    grep '^.ERROR' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.error 
+#    grep '^.WARNING' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.warning
+#    grep -v '^\[' $FILE > $FOLDER$NAME_FILE_WITHOUT_SLASH.raw 
+#done
 
-find Files/modbat.config.ConfigTest/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/modbat.config.ConfigTest/line_list
-find Files/config/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/config/line_list
-find Files/modbat/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/modbat/line_list
+#find Files/modbat.config.ConfigTest/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/modbat.config.ConfigTest/line_list
+#find Files/config/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/config/line_list
+#find Files/modbat/. -name '*.log.*' -o -name '*.err.*' | xargs cat | sort -u > Files/modbat/line_list
 
+function textreplace {
+    tr "[ \t]+" "\n" < $1line_list > $1word_list
+    sed -i "" "/^[ \t]*$/d" $1word_list
+    sed -i '' 's/[\.,{}:();]*$//' $1word_list
+    sed -i '' 's/^[\.,{}:();]*//' $1word_list
+    sort -u Files/modbat/word_list > $1word2_list 
+}
 
-tr " " "\n" < Files/modbat.config.ConfigTest/line_list > Files/modbat.config.ConfigTest/word_list
+function remove {
+    cp $1word2_list $1word_list
+    rm $1word2_list
+}
+
+textreplace "Files/modbat.config.ConfigTest/"
+textreplace "Files/modbat/"
+textreplace "Files/config/"
+
+remove "Files/modbat.config.ConfigTest/"
+remove "Files/modbat/"
+remove "Files/config/"
